b3615a3a9959da91952e3f0b8efdeecbe209d8c3
Mego <Mego@users.noreply.github.com>
2025-12-06 23:40:48 -0600

    use clap for arg parsing, support file input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
-use std::env::args;
+use std::{fs::read_to_string, path::PathBuf};
 
 use pufferfish::program::Program;
 
+use clap::{Args, Parser};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(flatten)]
+    input: Input,
+}
+
+#[derive(Args)]
+#[group(required = true, multiple = false)]
+struct Input {
+    /// The file containing the program
+    #[arg(short, long, value_name = "FILE")]
+    file: Option<PathBuf>,
+
+    /// The program
+    code: Option<String>,
+}
+
 fn main() -> Result<(), anyhow::Error> {
-    let code = args().nth(1).unwrap();
+    let cli = Cli::parse();
+    let code = if let Some(input_file) = cli.input.file {
+        read_to_string(input_file)?
+    } else {
+        cli.input.code.unwrap()
+    };
     let mut program = Program::new(&code)?;
     loop {
         program.step();
